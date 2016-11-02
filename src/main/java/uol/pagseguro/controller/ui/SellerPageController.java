6a906e838f890866f2cff95c997800405e959022
Lucas Machado <machadolucas@me.com>
2016-11-02 08:17:41 -0200

    Receber MultiPart (imagem) de form

--- src/main/java/uol/pagseguro/controller/ui/SellerPageController.java
+++ src/main/java/uol/pagseguro/controller/ui/SellerPageController.java
@@ -1,10 +1,14 @@
 package uol.pagseguro.controller.ui;
 
+import org.glassfish.jersey.internal.util.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import uol.pagseguro.entity.ComandaEntity;
 import uol.pagseguro.entity.SellerEntity;
 import uol.pagseguro.service.ComandaService;
@@ -39,7 +43,7 @@ public class SellerPageController {
         return "index";
     }
 
-    @RequestMapping("/web/comanda/{idComanda}")
+    @RequestMapping(method = RequestMethod.GET, path = "/web/comanda/{idComanda}")
     public String detailComanda(final Model model, @PathVariable("idComanda") final String idComanda) {
 
         final SellerEntity sellerEntity = this.sellerService.findFirst();
@@ -50,20 +54,24 @@ public class SellerPageController {
         return "detailComanda";
     }
 
-    @RequestMapping("/web/comanda/{idComanda}/closeToPay")
-    public String closeToPay(final Model model, @PathVariable("idComanda") final String idComanda) throws Exception {
+    @RequestMapping(method = RequestMethod.POST, path = "/web/comanda/{idComanda}/closeToPay")
+    public String closeToPay(final Model model, @PathVariable("idComanda") final String idComanda, //
+                             @RequestParam("file") final MultipartFile file, //
+                             @RequestParam("value") final String valueString) throws Exception {
 
         final SellerEntity sellerEntity = this.sellerService.findFirst();
         final ComandaEntity comanda = this.comandaService.getComanda(sellerEntity.getEmail(), idComanda);
 
-        this.comandaService.closeToPayment(idComanda, sellerEntity.getEmail(), new BigDecimal("15,00"), null);
+        final String base64Image = Base64.encodeAsString(file.getBytes());
+        final BigDecimal valor = new BigDecimal(valueString);
+        this.comandaService.closeToPayment(idComanda, sellerEntity.getEmail(), valor, base64Image);
 
         model.addAttribute("vendedor", sellerEntity);
         model.addAttribute("comanda", comanda);
         return "detailComanda";
     }
 
-    @RequestMapping("/web/comanda/{idComanda}/finalize")
+    @RequestMapping(method = RequestMethod.GET, path = "/web/comanda/{idComanda}/finalize")
     public String finalize(final Model model, @PathVariable("idComanda") final String idComanda) throws Exception {
 
         final SellerEntity sellerEntity = this.sellerService.findFirst();
